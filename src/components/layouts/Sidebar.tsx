d4bd2471a44ff4249f6141463e381a6ab084cb9d
TungTvPh81102 <tungtvph46392@fpt.edu.vn>
2024-07-08 21:14:30 +0700

    reject routes

--- src/components/layouts/Sidebar.tsx
+++ src/components/layouts/Sidebar.tsx
@@ -1,10 +1,15 @@
+"use client";
 import { menuItems } from "@/constants";
 import ActiveLink from "../common/ActiveLink";
 import { TmenuItem } from "@/types";
-import { UserButton } from "@clerk/nextjs";
+import { useAuth, UserButton } from "@clerk/nextjs";
 import { ModeToggle } from "../common/ModeToggle";
+import Link from "next/link";
+import { IconUsers } from "../icons";
 
 const Sidebar = () => {
+  const { userId } = useAuth();
+
   return (
     <div className="p-5 border-r border-r-gray-200 bg-white dark:bg-grayDarker dark:border-opacity-10 flex flex-col">
       <a href="" className="font-bold text-3xl inline-block mb-5">
@@ -23,7 +28,16 @@ const Sidebar = () => {
       </ul>
       <div className="mt-auto flex items-center justify-end gap-5">
         <ModeToggle></ModeToggle>
-        <UserButton />
+        {!userId ? (
+          <Link
+            href="sign-in"
+            className="size-10 rounded-lg bg-primary text-white flex items-center justyfy-center"
+          >
+            <IconUsers />
+          </Link>
+        ) : (
+          <UserButton />
+        )}
       </div>
     </div>
   );
